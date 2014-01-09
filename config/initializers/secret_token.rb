f826d4464ae0e4080e1faf8eb6743ace429702a4
Hunty Dumpty <hunter.horsley@gmail.com>
2014-01-08 21:56:10 -0800

    new repo rails 3.2.3 w pg on production and devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaptainsLog::Application.config.secret_token = 'b79d2b63ffa584dc1620d26ac79f0f64ef286c8810896beeab0a493c7946d552b6378355b60d208b212654fe5aead2d3d120961c35d8e53c8b7bc952b90b335b'
